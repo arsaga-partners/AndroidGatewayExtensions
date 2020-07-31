45f5d723576c84fc1deadbb4e6dc7183d0abc68e
内田正輝 <masaki.u@arsaga.jp>
2020-07-31 16:09:20 +0900

    Contextを受け取るよりサブクラスでSharedPreferencesを定義する方がシンプルになるので修正

--- src/main/java/jp/co/arsaga/extensions/gateway/BaseLiveSharedPreference.kt
+++ src/main/java/jp/co/arsaga/extensions/gateway/BaseLiveSharedPreference.kt
@@ -1,18 +1,14 @@
 package jp.co.arsaga.extensions.gateway
 
-import android.content.Context
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 
 abstract class BaseLiveSharedPreference<T : Any>(
-    fileName: String,
     private val key: String,
     private val defaultValue: T
 ) : LiveData<T>() {
 
-    abstract val context: Context
-
-    protected open val sharedPreference by lazy { context.getSharedPreferences(fileName, Context.MODE_PRIVATE) }
+    protected abstract val sharedPreference: SharedPreferences
 
     private val sharedPreferenceChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
         if (this.key == key) get(sharedPreferences, key, defaultValue)
